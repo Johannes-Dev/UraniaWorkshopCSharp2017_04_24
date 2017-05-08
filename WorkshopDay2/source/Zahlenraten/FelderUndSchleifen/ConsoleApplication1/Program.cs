e20d6f78b95ece21f9f72ae07cb2864317370ccf
Johannes <Johannes@DESKTOP-LTTBHHS>
2017-05-08 21:46:10 +0200

    Arrays und felder

---
+++ WorkshopDay2/source/Zahlenraten/FelderUndSchleifen/ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var feld = new int[5,5];
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    feld[i, j] = 0;
+                }
+            }
+
+            feld[2, 3] = 1;
+            
+            ausgabe(feld);
+
+
+
+            Console.ReadKey();
+
+        }
+
+
+        private static  void ausgabe(int[,] feld)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(feld[i, j]);
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
