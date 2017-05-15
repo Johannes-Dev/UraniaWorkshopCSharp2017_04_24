bb58121c4f32991a236c08f1223d03527fcc5237
Johannes <kontakt@johanneskoehler.de>
2017-05-15 20:12:29 +0200

    Ausgabe der Autos mit unter 50000 KM

--- WorkshopDay3/Code/KlasseBeispiel/KlasseBeispiel/Program.cs
+++ WorkshopDay3/Code/KlasseBeispiel/KlasseBeispiel/Program.cs
@@ -13,15 +13,26 @@ namespace KlasseBeispiel
 
             Auto audi = new Auto();
             audi.KilometerStand = 100000;
+            audi.Marke = "Audi";
             audi.Fahren(120);
 
             Auto bmw = new Auto();
             bmw.KilometerStand = 20000;
+            bmw.Marke = "BMW";
             bmw.Fahren(25);
 
+            var autos = new Auto[2];
+            autos[0] = audi;
+            autos[1] = bmw;
 
+            for (int i= 0;i<2;i++)
+            {
+                if(autos[i].KilometerStand<50000)
+                {
+                    Console.WriteLine("Das Auto mit der Marke " + autos[i].Marke + " hat wenniger als 50000 Kilometer gefahren;");
+                }
+            }
 
-            Console.WriteLine("Der BMW hat "+bmw.KilometerStand+"km auf dem Tacho");
             Console.ReadKey();
 
         }
