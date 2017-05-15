571dbcebfcad9de70ec314a6faae7aadaef3cc6a
Johannes <kontakt@johanneskoehler.de>
2017-05-15 19:58:35 +0200

    Beispiel Klassen

---
+++ KlasseBeispiel/KlasseBeispiel/Auto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasseBeispiel
+{
+    class Auto
+    {
+        public Auto()
+        {
+
+        }
+
+        public string Marke { get; set; }
+
+        public int PS { get; set; }
+
+        public int KilometerStand { get; set; }
+
+        private int m_AnzahlSitze;
+
+        public int AnzahlSitze
+        {
+            get
+            {
+                return m_AnzahlSitze;
+            }
+            set           
+            {
+                m_AnzahlSitze = value;
+            }
+        }
+
+        public void Fahren (int Strecke)
+        {
+            KilometerStand = Strecke + KilometerStand;
+        }
+
+
+
+
+    }
+}
---
+++ KlasseBeispiel/KlasseBeispiel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasseBeispiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Auto audi = new Auto();
+            audi.KilometerStand = 100000;
+            audi.Fahren(120);
+
+            Auto bmw = new Auto();
+            bmw.KilometerStand = 20000;
+            bmw.Fahren(25);
+
+
+
+            Console.WriteLine("Der BMW hat "+bmw.KilometerStand+"km auf dem Tacho");
+            Console.ReadKey();
+
+        }
+    }
+}
